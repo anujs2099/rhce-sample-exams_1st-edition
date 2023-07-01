185879e1318562c071bf4ea28428c71dae3e18a6
Anuj Saxena <anuj.saxena01@hotmail.com>
2023-07-01 16:59:13 -0400

    updated files

--- solutions/lab3/task1/task1.sh
+++ solutions/lab3/task1/task1.sh
@@ -14,3 +14,14 @@ ansible-doc debug | grep EXAMPLES -A3000 | less
 vi facts.yaml
 ansible-playbook --syntax-check facts.yaml 
 ansible-playbook facts.yaml 
+
+vi ipfact.yaml
+ansible-doc debug | grep '^='
+ansible-doc debug | grep EXAMPLES -A3000 | less
+vi ipfact.yaml
+ansible-playbook facts.yaml | less
+# type /ip
+# type n multiple times 
+vi ipfact.yaml
+ansible-playbook --syntax-check ipfact.yaml
+ansible-playbook ipfact.yaml
