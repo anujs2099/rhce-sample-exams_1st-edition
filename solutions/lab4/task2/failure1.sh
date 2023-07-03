d02c97f29bfacbaf207c2c39f4e0a596d2c5bc5e
Anuj Saxena <anuj.saxena01@hotmail.com>
2023-07-03 13:10:45 -0400

    updated files

--- solutions/lab4/task2/failure1.sh
+++ solutions/lab4/task2/failure1.sh
@@ -18,5 +18,5 @@ ansible-playbook failure1.yaml
 # ignore_errors: boolean --> per task / continue the playbook even if a task fails
 # force_handlers: boolean --> per play / run handlers even if a task fails
 # failed_when: condition --> per task / register variable must be used / exit the playbook
-# fail module --> ignore_errors must be set to True / register variable must be used
-# 
+# fail module --> fails the task if condition is met / if condition is not met, skips the task
+# debug module --> succeeds the task if condition is met / if condition is not met, skips the task
--- solutions/lab4/task2/failure2.sh
+++ solutions/lab4/task2/failure2.sh
@@ -7,9 +7,11 @@ cp ../../lab1/task2/inventory .
 cp ../../lab1/task2/ansible.cfg .
 
 vi failure2.yaml
-ansible-doc shell | grep EXAMPLES -A3000 | less
+ansible-doc lvol | grep EXAMPLES -A3000 | less
 vi failure2.yaml
 ansible-doc debug | grep EXAMPLES -A3000 | less
+vi failure2.yaml
+ansible-doc fail | grep EXAMPLES -A3000 | less
 vi failure2.yaml
 ansible-playbook --syntax-check failure2.yaml
 ansible-playbook failure2.yaml
@@ -18,5 +20,5 @@ ansible-playbook failure2.yaml
 # ignore_errors: boolean --> per task / continue the playbook even if a task fails
 # force_handlers: boolean --> per play / run handlers even if a task fails
 # failed_when: condition --> per task / register variable must be used / exit the playbook
-# fail module --> ignore_errors must be set to True / register variable must be used
-# 
+# fail module --> fails the task if condition is met / if condition is not met, skips the task
+# debug module --> succeeds the task if condition is met / if condition is not met, skips the task
