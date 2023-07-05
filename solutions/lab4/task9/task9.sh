14983b22b87e6b32bfd72c33d15863afcbc22452
Anuj Saxena <anuj.saxena01@hotmail.com>
2023-07-04 21:29:51 -0400

    updated files

--- solutions/lab4/task9/task9.sh
+++ solutions/lab4/task9/task9.sh
@@ -7,15 +7,16 @@ cp ../../lab1/task2/inventory .
 cp ../../lab1/task2/ansible.cfg .
 
 vi task9.yaml
+ansible-doc yum | grep EXAMPLES -A3000 | less
+ansible-doc service | grep EXAMPLES -A3000 | less
+ansible-doc file | grep EXAMPLES -A3000 | less
+ansible-doc sefcontext | grep EXAMPLES -A3000 | less
+ansible-doc seboolean | grep EXAMPLES -A3000 | less
+ansible-doc firewalld | grep EXAMPLES -A3000 | less
 ansible-doc template | grep EXAMPLES -A3000 | less
-vi task9.yaml
 ansible-doc shell | grep EXAMPLES -A3000 | less
-vi task9.yaml
 ansible-doc debug | grep EXAMPLES -A3000 | less
-vi task9.yaml
 ansible-playbook --syntax-check task9.yaml
 ansible-playbook task9.yaml
 
-## Using Control Structures in Jinja2 Templates
-# Not found easily in ansible documentation
-# for & if statements
+## Comments
